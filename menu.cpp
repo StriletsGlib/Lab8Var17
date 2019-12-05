e82d5882399cd0aca642ebf1d801586bd5b10655
StriletsGlib <58534572+StriletsGlib@users.noreply.github.com>
2019-12-05 23:12:10 +0200

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -10,6 +10,7 @@ void listOfCommands(){
     cout<<"write1f: reads first list of doubles to file"<<endl;
     cout<<"write2f: reads second list of doubles to file"<<endl;
     cout<<"mCreating: create new list, merging l1 and l2 with incriasing still existing"<<endl;
+    cout<<"mCharge: merge both of lists into the first one"<<endl;
     cout<<"readResF: write resultat to a file"<<endl;
     cout<<"end: end the program"<<endl;
     cout<<"help: gives list of commands with descriptions"<<endl;
